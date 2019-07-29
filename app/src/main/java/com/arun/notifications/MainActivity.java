1d4d4ba59dd0d632a044c9dba81ac930a1477044
ARUN RAJ KUMAR <arunrkwork@gmail.com>
2019-07-29 11:21:03 +0400

    Add action button to notification

--- app/src/main/java/com/arun/notifications/MainActivity.java
+++ app/src/main/java/com/arun/notifications/MainActivity.java
@@ -47,6 +47,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
+        Intent yesIntent = new Intent(this, YesActivity.class);
+        yesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent yesPendingIntent = PendingIntent.getActivity(this, 0, yesIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Intent noIntent = new Intent(this, NoActivity.class);
+        noIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent noPendingIntent = PendingIntent.getActivity(this, 0, noIntent, PendingIntent.FLAG_ONE_SHOT);
+
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
         builder.setSmallIcon(R.drawable.ic_sms_black_24dp);
@@ -55,6 +63,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
         builder.setAutoCancel(true);
         builder.setContentIntent(pendingIntent);
+        builder.addAction(R.drawable.ic_done_black_24dp, "YES", yesPendingIntent);
+        builder.addAction(R.drawable.ic_clear_black_24dp, "NO", noPendingIntent);
+
+
+
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
