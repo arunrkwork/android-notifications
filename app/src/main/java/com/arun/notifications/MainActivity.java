f275d62c5472e56b48374ea9140b8f8922a8f1b1
ARUN RAJ KUMAR <arunrkwork@gmail.com>
2019-07-29 13:24:09 +0400

    Notification with progress bar

--- app/src/main/java/com/arun/notifications/MainActivity.java
+++ app/src/main/java/com/arun/notifications/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         createNotificationChannel();
 
 
-        Intent intent = new Intent(this, NotificationActivity.class);
+       /* Intent intent = new Intent(this, NotificationActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
@@ -56,20 +56,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Intent noIntent = new Intent(this, NoActivity.class);
         noIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent noPendingIntent = PendingIntent.getActivity(this, 0, noIntent, PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent noPendingIntent = PendingIntent.getActivity(this, 0, noIntent, PendingIntent.FLAG_ONE_SHOT);*/
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_sms_black_24dp);
-        builder.setContentTitle("Notification Example");
-        builder.setContentText("This is a notification");
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_get_app_black_24dp);
+        builder.setContentTitle("Image Download");
+        builder.setContentText("Download in progress");
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setAutoCancel(true);
-        builder.setContentIntent(pendingIntent);
+        //builder.setAutoCancel(true);
+        //builder.setContentIntent(pendingIntent);
 //        builder.addAction(R.drawable.ic_done_black_24dp, "YES", yesPendingIntent);
 //        builder.addAction(R.drawable.ic_clear_black_24dp, "NO", noPendingIntent);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             RemoteInput remoteInput = new RemoteInput.Builder(TXT_REPLY)
                     .setLabel("Reply")
                     .build();
@@ -84,10 +84,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             builder.addAction(action);
         }
+*/
 
+       final int maxProgress = 100;
+       int currentProgress = 0;
 
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+       builder.setProgress(maxProgress, currentProgress, false);
+
+        final NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                int count = 0;
+
+                while (count <= 100){
+                    count = count + 10;
+                    try {
+                        sleep(1000);
+                        builder.setProgress(maxProgress, count, false);
+                        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                builder.setContentText("Download Completed");
+                builder.setProgress(0, 0, false);
+                notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+            }
+        };
+
+        thread.start();
 
     }
 
